faaa6dc768f9f4dd12c2f29bafc920e5b7d5f226
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-08-21 00:05:29 +0530

    Group related attributes to show together in the TUI

--- httpCaller.py
+++ httpCaller.py
@@ -1,12 +1,21 @@
 import requests
 import typer
 import json
-from rich.console import Console
+from rich.console import Console,Group
 from rich.syntax import Syntax
+from rich.panel import Panel
+from rich.layout import Layout
+from rich.text import Text
 
 app = typer.Typer()
 console = Console()
 
+layout = Layout()
+
+layout.split_column(
+    Layout(name="request"),
+    Layout(name="response")
+)
 @app.command()
 def request(
     method:str,
@@ -22,20 +31,49 @@ def request(
         except ValueError:
             console.print("[red]Invalid Headers! Use key:value format.[/red]")
             raise typer.Exit()
-        
+    request_content = Group(
+        Text(f"Method:{method}"),
+        Text(f"URL:{url}"),
+        Text(getData(data)),
+        Text(getHeader(headers))
+    )
+    layout["request"].update(Panel(request_content,title="Request Data"))
     try:
         response = requests.request(method=method.upper(),url=url,headers=headers,data=data)
-        console.print(f"[bold green]{response.status_code}[/bold green]")
 
         try:
             json_obj = response.json()
             json_text = json.dumps(json_obj, indent=2)
             syntax = Syntax(json_text,"json",theme="monokai",line_numbers=True)
-            console.print(syntax)
+            response_content = Group(
+                Text(f"Status Code:{response.status_code}"),
+                syntax
+            )
+            layout["response"].update(Panel(response_content,title="Response Data"))
         except Exception:
-            console.print(response.text)
+            response_content = Group(
+                Text(f"Status Code:{response.status_code}"),
+                Text(f"Resonse Message:{response.text}")
+            )
+            layout["response"].update(Panel(response_content,title="Response Data"))
     except Exception as e:
-        console.print(f"[red]Request failed due to: {e}[/red]")
+        response_content = Group(
+            Text(f"Exception:{e}")
+        )
+        layout["response"].update(Panel(response_content,title="Response Data"))
+    console.print(Panel(layout, title="HttpCaller"))
+
+def getData(data):
+    if data:
+        return f"Data:{data}"
+    else:
+        return "NULL"
+
+def getHeader(headers):
+    if headers:
+        return f"Header:{headers}"
+    else:
+        return "NULL"
 
 if __name__ == "__main__":
     app()
